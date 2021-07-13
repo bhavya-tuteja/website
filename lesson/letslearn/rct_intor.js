48311ab7c619cdb27f85716ee5e45e7246332da6
rramangrover <ramansirji56@gmail.com>
2021-07-13 19:19:02 +0530

    addrct1

---
+++ lesson/letslearn/rct_intor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import reactDOM from ' react-dom';
+class Test extends React.Component{
+    render() {
+        return <h1>Good Day, Sir.</h1>
+
+    }
+}
+reactDOM.render(<Test/>), document.getElementById('root'); 
