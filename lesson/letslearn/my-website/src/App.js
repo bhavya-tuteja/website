aa4610a1fe95d73fe615ad1cc27511ca8d8c4227
rramangrover <ramansirji56@gmail.com>
2021-07-13 23:19:05 +0530

    addrct3

---
+++ lesson/letslearn/my-website/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Good Day</h1>
+      <h2>Oh! Yes, Good Day</h2>
+    </div>
+   
+  );
+}
+
+export default App;
+
+// 
+{/* <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Good Day, Sir.</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */}
