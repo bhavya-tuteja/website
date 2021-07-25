43701506c96f8cdf73b278bdb2e835e9a330697e
suraj-kumar-sunny <suraj.kumarengg14321@gmail.com>
2021-07-25 21:22:13 +0530

    error solved

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const MyApp = () => {
     return (
         <div>
-            MyApp
+            <h1>Hello Miss Bhavya your issue is solved now...</h1>
         </div>
     )
 }
