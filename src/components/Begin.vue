da95abc252a216b857932342bda22bb1977600a0
bhavya-tuteja <tutejabhavya1234@gmail.com>
2021-07-31 12:05:33 +0530

    add begin

---
+++ src/components/Begin.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="form">
+    <label for="email" class="mr-sm-2">Email address:</label>
+  <input type="email" class="form-control mb-2 mr-sm-2" placeholder="Enter email" id="email">
+  <label for="pwd" class="mr-sm-2">Password:</label>
+  <input type="password" class="form-control mb-2 mr-sm-2" placeholder="Enter password" id="pwd">
+  <div class="form-check mb-2 mr-sm-2">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary mb-2">Submit</button>
+</form>
+</div>
+
+</template>
+<script>
+import { Begin } from "./components/Abc.vue";
+
+
+export default  {
+  name: 'Begin',
+  props: {
+    msg: String
+  }
+}
+</script>
