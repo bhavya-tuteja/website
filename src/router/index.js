3d829920340444d3142565e1899c8b811194ef6e
SonyDaman <sonydaman@gmail.com>
2021-08-02 15:35:15 +0530

    add lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
-import TodoList from "@/views/TodoList.vue";
+// import TodoList from "@/views/TodoList.vue";
 
 const routes = [
   {
@@ -11,7 +11,8 @@ const routes = [
   {
     path: "/todo",
     name: "Todo",
-    component: TodoList,
+    // component: TodoList,
+    component: () => import("@/views/TodoList.vue"),
   },
   {
     path: "/about",
@@ -20,7 +21,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
   },
 ];
 
